bb8ba70570d77897d6be6cf0924b9dc0f2a8116d
Blake Ehler <ehlerblake@gmail.com>
2020-09-18 18:48:35 -0700

    added changes

--- public/js/buy.js
+++ public/js/buy.js
@@ -6,13 +6,12 @@ $(document).ready(() => {
         let searchName = $(".input").val()
         $.get("/api/album_data/" + searchName).then(data => {
             console.log(data)
-                //data.albums.items.forEach(element => console.log(element))
             let artist = data.albums.items[0].artists[0].name;
             let release = data.albums.items[0].release_date
             let albumName = data.albums.items[0].name
             let albumCoverM = data.albums.items[0].images[1].url
             let url = 'https://open.spotify.com/artist/' + data.albums.items[0].artists[0].id
-            $(".hello").text(artist);
+
         });
     });
     
