2ab4584e574cbc210e49251c09397a1fec3cb3d6
Matt <64766063+mbone1@users.noreply.github.com>
2020-09-21 14:22:37 -0700

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,67 +2,42 @@
 const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-const sales = require("../models/sales");
-//const Album = require('../models/album');
+
 
 module.exports = function(app) {
 
-    app.get('/', (req, res) => {
-        // res.json(path.join(__dirname, "index"));
-
-        res.render('index');
-    });
-
-    app.get('/sell', (req, res) => {
-        res.render('sell');
-    });
-    app.get('/buy', (req, res) => {
-        res.render('buy');
+    app.get('/signup', (req, res) => {
+        res.render('signup');
     });
     app.get('/login', (req, res) => {
         res.render('login');
     });
+    app.get('/signup', (req, res) => {
+        res.render('signup');
+    });
+
+    app.get("/", function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            return res.render("index");
+        }
+        res.render("login");
+    });
+
+    app.get("/sell", isAuthenticated, function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            return res.render("sell");
+        }
+        res.render("login");
+    });
+    app.get("/buy", isAuthenticated, function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            return res.render("buy");
+        }
+        res.render("login");
+    });
+
 };
-
-// app.get("/signup", (req, res) => {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//         res.redirect("/members");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/signup.html"));
-// });
-
-
-// app.get("/members", isAuthenticated, (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/members.html"));
-// });
-
-// app.get("/signup", (req, res) => {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//         res.redirect("/sell");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/signup.html"));
-// });
-
-
-
-// app.get('/buy', (req, res) => {
-//     /* Album.buyAlbum(albums => {
-//       res.render('/buy', {
-//         albums: albums
-//       })
-//     })*/
-//     //conection.query().then(data => res.render('/buy', {album: data}))
-//     res.render('buy', {
-//         albums: [{ 'artist': 'artist' }]
-//     });
-
-// });
-
-// });
-
-
-}
-
 
